f8d9470add3f6af5350ca0aa6ef526664bf2a789
corphish <d97.avinaba@gmail.com>
2016-07-20 11:32:01 +0530

    taoshan: Use my old build script
    Signed-off-by: corphish <d97.avinaba@gmail.com>

--- build.sh
+++ build.sh
@@ -1,60 +1,108 @@
- #
- # Copyright � 2014, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
- #
- # Custom build script
- #
- # This software is licensed under the terms of the GNU General Public
- # License version 2, as published by the Free Software Foundation, and
- # may be copied, distributed, and modified under those terms.
- #
- # This program is distributed in the hope that it will be useful,
- # but WITHOUT ANY WARRANTY; without even the implied warranty of
- # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- # GNU General Public License for more details.
- #
- #
 #!/bin/bash
-#Tuneables
-STRIP="/home/corphish/android/toolchains/linaro/linaro-4.9.4/bin/arm-eabi-strip"
-ZIMAGE="/home/corphish/android/kernel/taoshan/5.1/arch/arm/boot/zImage"
-KERNEL_DIR="/home/corphish/android/kernel/taoshan/5.1"
-ZIP_DIR="/home/corphish/android/kernel/taoshan/5.1/zip/raw"
-KERNEL="zImage"
 
-#Main
-echo "Starting build..."
-BUILD_START=$(date +"%s")
-export ARCH=arm
-export SUBARCH=arm
-export CROSS_COMPILE=/home/corphish/android/toolchains/gcc/4.8/bin/arm-eabi-
-export KBUILD_BUILD_USER="avinaba"
-export KBUILD_BUILD_HOST="build"
-echo "Cleaning.."
-if [ -f $KERNEL_DIR/arch/arm/boot/zImage ];
-then
-rm $ZIMAGE
-fi
-echo "Building..."
-make cyanogenmod_taoshan_defconfig
-make
-if [ -f $ZIMAGE ];
-then
-echo "Copying kernel.."
-cp $KERNEL_DIR/arch/arm/boot/$KERNEL $ZIP_DIR/tools/zImage
-echo "Copying modules.."
-rm $ZIP_DIR/system/lib/modules/*
-find . -name '*.ko' -exec cp {} $ZIP_DIR/system/lib/modules \;
-cd $ZIP_DIR/system/lib/modules
-echo "Stripping modules for size.."
-$STRIP --strip-unneeded *.ko
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-echo "Zipping.."
-cd $KERNEL_DIR
-./zip.sh
-echo "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-else
-echo "Compilation failed! Fix the errors!"
-fi
+# Defaults
+ARCH=arm
+DEFCONFIG=cyanogenmod_taoshan_defconfig
+DEVICE=taoshan
+LOCAL_PATH=$(pwd)
+KERNEL=zImage
+KERNEL_PATH=$LOCAL_PATH/arch/arm/boot/$KERNEL
+ZIP_DIR=$LOCAL_PATH/zip/raw
 
+# Tuneables
+CROSS_COMPILE_LOCATION=/home/boo/android/system/cm-13.0/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
+CROSS_COMPILE=$CROSS_COMPILE_LOCATION/bin/arm-eabi-
+STRIP=$CROSS_COMPILE_LOCATION/bin/arm-eabi-strip
+ZIP_PREFIX=zd
 
+# Colors
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+warn() {
+	echo -e $yellow"Warning:"$nocol $@
+}
+
+error() {
+	echo -e $red"Error:"$nocol $@
+}
+
+info() {
+	echo -e $blue"Info:"$nocol $@
+}
+
+install() {
+	echo -e $cyan"Install:"$nocol $@
+}
+
+export_variables() {
+	export ARCH=$ARCH
+	export CROSS_COMPILE=$CROSS_COMPILE
+}
+
+check_environment() {
+	if [ -f $KERMEL_PATH ];
+	then
+		warn "You are making a dirty build"
+	else
+		info "Making a clean build"
+	fi
+	if [ -d $ZIP_DIR ];
+	then
+		info "Template zip directory found, proceeding..."
+	else
+		error "Template zip directory not found. Aborting!!"
+		exit
+	fi	
+}
+
+clean() {
+	make clean
+	rm -rf .version
+}
+
+build_kernel() {
+	make $DEFCONFIG
+	make
+}
+
+collect_files() {
+	cp $KERNEL_PATH $ZIP_DIR/tools/
+	find . -name '*.ko' -exec cp {} $ZIP_DIR/system/lib/modules \;
+}
+
+strip() {
+	cd $ZIP_DIR/system/lib/modules
+	$STRIP --strip-unneeded *.ko
+}
+
+make_zip() {
+	# TODO: Better do python and use the technique used by Android build system.
+	cd $ZIP_DIR
+	zip -r ../archives/$ZIP_PREFIX-$(date +"%Y%m%d")-$DEVICE.zip *
+}
+
+wrapper() {
+	if [ "$1" == "clean" ];
+	then info "Cleaning" && clean
+	fi
+	check_environment
+	export_variables
+	info "Building kernel.."
+	build_kernel
+	if [ ! -f $KERNEL_PATH ];
+	then error "Build failed! Please fix the errors!" && exit
+	fi
+	info "Gathering all necessary files"
+	collect_files
+	info "Stripping modules"
+	strip
+	info "Making flashable zip"
+	make_zip
+	install $LOCAL_PATH/zip/archives/$ZIP_PREFIX-$(date +"%Y%m%d")-$DEVICE.zip
+}
+
+wrapper $1
